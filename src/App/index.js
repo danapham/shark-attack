0046475d3df9815e1f05feb1b511bae7a990025c
Dana Pham <danapham12@gmail.com>
2020-11-10 21:38:40 -0600

    working on follow the light function

--- src/App/index.js
+++ src/App/index.js
@@ -3,12 +3,30 @@ import './App.scss';
 import studentData from '../helpers/data/studentData';
 
 class App extends React.Component {
+  state = {
+    alives: [],
+    deads: [],
+  }
+
   render() {
-    studentData.getData().then((res) => console.warn(res));
+    studentData.livingStudents().then((res) => this.setState({
+      alives: res,
+    }));
+
+    studentData.dearlyBeloved().then((res) => this.setState({
+      deads: res,
+    }));
+
+    // const { students } = this.state;
+    // const displayStudents = () => (
+    //   students.map((student) => (
+    //     console.warn(students)
+    //   ))
+    // );
+
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <button className="btn btn-info">Shark Attack</button>
       </div>
     );
   }
